85d07e58fcb90c70b6123308076cf655766961c5
son-hunseo <thsgnstj0512@gmail.com>
2026-01-05 22:56:44 +0900

    [app - bugfix] 라이트모드 고정
    - 다크모드가 고려되어있지 않은데, 기기가 다크모드일 경우 앱 UI 일부가 다크모드로 되어버려 이상해진다. 이에 이를 라이트모드로 고정한다.

--- climbear-app/app/src/main/java/com/example/climbear/MainActivity.kt
+++ climbear-app/app/src/main/java/com/example/climbear/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
 
         setContent {
-            ClimbearTheme {
+            ClimbearTheme(darkTheme = false) {
                 ClimbearApp()
             }
         }
